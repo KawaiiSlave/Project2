4124b8d0709b20be88780951bb4ba24b9472e9c1
Jackson Jewell <jackson.t.jewell@gmail.com>
2020-02-11 19:28:09 -0600

    Soft reset

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,10 +8,10 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function(app) {}
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/", function(req, res) {})
     
