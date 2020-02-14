d40964598e3d750b50408c0505736320797a93b3
Tyler <tgandle@gmail.com>
2020-02-13 20:05:20 -0600

    added some things into handlebars

--- server.js
+++ server.js
@@ -20,6 +20,28 @@ app.set("view engine", "handlebars");
 var routes = require("./routes/controller.js");
 app.use(routes);
 
+// Each of the below routes just handles the HTML page that the user gets sent to.
+  app.get("/Nozama", function(req, res) {
+      res.render("index", {title: "Home Page"});
+});
+
+  app.get("/books", function(req, res) {
+    res.render("item", {title: "Card catalogue"});
+});
+
+app.get("/cart", function(req, res) {
+    res.render("cart", {title: "Your cart"});
+});
+
+app.get("/admin", function(req, res) {
+    res.render("admin", {title: "Administrator"});
+});
+
+app.get("/search", function(req, res) {
+    res.render("search", {title: "Searching for books"});
+});
+
+
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
