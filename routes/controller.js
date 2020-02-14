e0f097be0ec18f48d1416fde7a9fb511be851532
Jackson Jewell <jackson.t.jewell@gmail.com>
2020-02-13 19:55:38 -0600

    Created routes

--- routes/controller.js
+++ routes/controller.js
@@ -2,12 +2,59 @@ let express = require("express");
 
 let router = express.Router();
 
-let books = require("../models/books.js");
-let cart = require("../models/cart.js");
-let user = require("../models/user.js");
+let db = require("../models");
 
 router.get("/", function(req, res) {
- 
+    db.Books.findAll({}).then(function(data) {
+        let booksItem = {
+            book: data
+        }
+        res.render("index", booksItem);
+      });
+});
+
+router.get("/login/:username", function(req, res) {
+    db.Users.findAll({where:{
+        username: req.params.username
+    }}).then(function(dbUser) {
+        res.json(dbUser);
+      });
+});
+
+router.get("/search/:searchItem", function(req, res) {
+    db.Books.findAll({where:{
+        [Books.or]:[
+          {title: req.params.searchItem},
+          {author: req.params.searchItem}
+        ]
+    }}).then(function(data) {
+        let booksItem = {
+            book: data
+        }
+        res.render("search", booksItem);
+      });
+});
+
+router.get("/search/category/:searchItem", function(req, res) {
+    db.Books.findAll({where:{
+          category: req.params.searchItem
+    }}).then(function(data) {
+        let booksItem = {
+            book: data
+        }
+        res.render("search", booksItem);
+      });
+});
+
+router.get("/item/:itemId", function(req, res) {
+    db.Books.findAll({where:{
+        id: req.params.itemId
+    }}).then(function(data){
+        let booksItem = {
+            book: data
+        }
+        res.render("item", booksItem);
+    });
 });
 
 router.post("/api/books", function(req, res) {
@@ -19,7 +66,11 @@ router.put("/api/books/:id", function(req, res) {
 });
 
 router.delete("/api/books/:id", function(req, res) {
- 
+    db.Books.destroy({where:{
+        id: req.params.itemId
+    }}).then(function(dbBooks){
+        res.json(dbBooks);
+    });
 });
 
 module.exports = router;
