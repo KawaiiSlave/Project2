9969bdb56ff6c3c23bc1ca5cf19984b97d896596
Tyler <tgandle@gmail.com>
2020-02-13 18:29:40 -0600

    commiting files

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
+const express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
+const app = express();
 
-// Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+  app.get("/Nozama", function(req, res) {
+      res.render("index", {title: "Home Page"});
 });
+
+  app.get("/books", function(req, res) {
+    res.render("item", {title: "Card catalogue"});
+});
+
+app.get("/cart", function(req, res) {
+    res.render("cart", {title: "Your cart"});
+});
+
+app.get("/admin", function(req, res) {
+    res.render("admin", {title: "Administrator"});
+});
+
+app.get("/search", function(req, res) {
+    res.render("search", {title: "Searching for books"});
+});
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
