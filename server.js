0f6e14f88ffa103b2e49ce80930976768fad8319
Jackson Jewell <jackson.t.jewell@gmail.com>
2020-02-13 21:05:39 -0600

    Got server to function

--- server.js
+++ server.js
@@ -20,30 +20,6 @@ app.set("view engine", "handlebars");
 var routes = require("./routes/controller.js");
 app.use(routes);
 
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
-
-  app.get("/books", function(req, res) {
-    res.render("item", {title: "Card catalogue"});
-});
-
-app.get("/cart", function(req, res) {
-    res.render("cart", {title: "Your cart"});
-});
-
-app.get("/admin", function(req, res) {
-    res.render("admin", {title: "Administrator"});
-});
-
-app.get("/search", function(req, res) {
-    res.render("search", {title: "Searching for books"});
-});
-
-
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
